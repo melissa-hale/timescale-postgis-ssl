c6cc99c82f3eca3b8df33b90c2d8c6042a72a192
Melissa Hale <melissakayh@gmail.com>
2023-11-03 12:23:20 -0500

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -3,7 +3,7 @@
 # set the PGHOST to localhost
 # this is specific to Railway and allows
 # us to use PGHOST after the init
-export PGHOST="localhost"
+export PGHOST="127.0.0.1"
 export PGPORT=5432
 
 # Call the entrypoint script with the
